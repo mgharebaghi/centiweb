ba7521e7da123cdc9f3648dafc538237d3ec16a5
mohammad <mgharebaghi82@gmail.com>
2024-06-27 22:13:56 +0330

    check bug in get apis

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -8,30 +8,34 @@ const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 export async function POST(req: Request, res: NextApiResponse) {
-  const db = client.db("centiweb");
-  const collection = db.collection<WithId<Admin>>("admins");
-  const { email, password } = await req.json();
-  const document = await collection.findOne({ email: email });
+  try {
+    const db = client.db("centiweb");
+    const collection = db.collection<WithId<Admin>>("admins");
+    const { email, password } = await req.json();
+    const document = await collection.findOne({ email: email });
 
-  if (document) {
-    if (document.password === password) {
-      cookies().set("loggedin", "true", {
-        expires: new Date(Date.now() + 60 * 60 * 24 * 1000), //save loged in data into cookies for 24 hours
-      });
-      return NextResponse.json({
-        status: "success",
-        description: "done",
-      });
+    if (document) {
+      if (document.password === password) {
+        cookies().set("loggedin", "true", {
+          expires: new Date(Date.now() + 60 * 60 * 24 * 1000), //save loged in data into cookies for 24 hours
+        });
+        return NextResponse.json({
+          status: "success",
+          description: "done",
+        });
+      } else {
+        return NextResponse.json({
+          status: "error",
+          description: "password",
+        });
+      }
     } else {
       return NextResponse.json({
-        status: "error",
-        description: "password",
+        status: "notfound",
+        description: "user not found",
       });
     }
-  } else {
-    return NextResponse.json({
-      status: "notfound",
-      description: "user not found",
-    });
+  } catch (e) {
+    console.log(e);
   }
 }
