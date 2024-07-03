8c973aebafee7aa9fde596d603e67d69a3ef594d
mohammad <mgharebaghi82@gmail.com>
2024-07-03 13:44:42 +0330

    fixed image upload bug

--- app/api/delete/route.ts
+++ app/api/delete/route.ts
@@ -1,18 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import {MongoClient, ObjectId} from 'mongodb';
+import { MongoClient, ObjectId } from "mongodb";
 
 const uri = "mongodb://0.0.0.0:27017";
 const client = new MongoClient(uri);
 
 export async function POST(req: NextRequest) {
+  try {
     const db = client.db("centiweb");
     const collection = db.collection("posts");
     const data = await req.json();
     const id = new ObjectId(data.id);
 
-    await collection.deleteOne({"_id": id});
+    await collection.deleteOne({ _id: id });
 
     return NextResponse.json({
-        status: "done"
+      status: "done",
     });
+  } catch (e) {
+    console.log(e);
+  }
 }
