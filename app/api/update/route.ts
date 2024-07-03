b2283b5e69f61d3779df6b5d776ceb9eecd8297f
mohammad <mgharebaghi82@gmail.com>
2024-07-03 16:01:35 +0330

    fixed image upload bug

--- app/api/update/route.ts
+++ app/api/update/route.ts
@@ -1,25 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import { MongoClient, ObjectId } from "mongodb";
 
-const uri = "mongodb://localhost:27017";
+const uri = "mongodb://0.0.0.0:27017";
 const client = new MongoClient(uri);
 
 export async function POST(req: NextRequest) {
-  const db = client.db("centiweb");
-  const collection = db.collection("posts");
-  const post = await req.json();
-  const objectId = new ObjectId(post.id);
+  try {
+    const db = client.db("centiweb");
+    const collection = db.collection("posts");
+    const post = await req.json();
+    const objectId = new ObjectId(post.id);
 
-  const updatedData = {
-    title: post.title,
-    content: post.content,
-    description: post.description,
-    image: post.image,
-  };
+    const updatedData = {
+      title: post.title,
+      content: post.content,
+      description: post.description,
+      image: post.image,
+    };
 
-  await collection.updateOne({ _id: objectId }, { $set: updatedData });
+    await collection.updateOne({ _id: objectId }, { $set: updatedData });
 
-  return NextResponse.json({
-    message: "success",
-  });
+    return NextResponse.json({
+      message: "success",
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
