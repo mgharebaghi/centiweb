838674397ef7bc10acb477bc0873e34173adfd1d
mohammad <mgharebaghi82@gmail.com>
2024-07-05 12:52:57 +0330

    set theme to light

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,21 @@ import Articles from "./components/articles.tsx";
 import Generated from "./components/generated";
 import Becomes from "./components/becomes";
 import Make from "./components/usedToMake";
+import { ThemeProvider as LightTheme } from "next-themes";
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
 
 export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <Banner />
-      <Articles />
-      <Generated />
-      <Becomes />
-      <Make />
-    </ThemeProvider>
+    <LightTheme attribute="class" defaultTheme="light">
+      <ThemeProvider theme={theme}>
+        <Banner />
+        <Articles />
+        <Generated />
+        <Becomes />
+        <Make />
+      </ThemeProvider>
+    </LightTheme>
   );
 }
