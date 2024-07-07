5de0e474fc779d2858961a86f8650439514df440
mohammad <mgharebaghi82@gmail.com>
2024-07-07 14:45:07 +0330

    add rpc and relays handeling

--- app/api/relays/route.ts
+++ app/api/relays/route.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { MongoClient, WithId } from "mongodb";
 import { Relays } from "../types/types";
+import { NextApiRequest } from "next";
 
 const uri = "mongodb://0.0.0.0:27017";
 const client = new MongoClient(uri);
+const db = client.db("centiweb");
+const collection = db.collection<WithId<Relays>>("relays");
 
 export async function POST(req: NextRequest) {
   try {
     let anothers: Relays[] = [];
     let request = await req.json();
-    let db = client.db("centiweb");
-    let collection = db.collection<WithId<Relays>>("relays");
     let doc = await collection.findOne({ addr: request.addr });
     let cursor = await collection.find();
 
@@ -36,6 +37,42 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({
       status: "error",
       detail: "server problem: " + e,
+    });
+  }
+}
+
+export async function GET() {
+  try {
+    let addresses: Relays[] = [];
+    let cursor = collection.find({});
+
+    await cursor.forEach((doc) => {
+      addresses.push({ addr: doc.addr });
+    });
+
+    return NextResponse.json({
+      status: "success",
+      data: addresses,
+    });
+  } catch (e) {
+    return NextResponse.json({
+      status: "error: " + e,
+      data: [],
+    });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const relay = req.nextUrl.searchParams.get("addr");
+    collection.deleteOne({ addr: relay?.toString() });
+
+    return NextResponse.json({
+      status: "success",
+    });
+  } catch (e) {
+    return NextResponse.json({
+      status: "error: " + e,
     });
   }
 }
--- app/api/rpc/route.ts
+++ app/api/rpc/route.ts
@@ -4,13 +4,13 @@ import { RPC } from "../types/types";
 
 const uri = "mongodb://0.0.0.0:27017";
 const client = new MongoClient(uri);
+const db = client.db("centiweb");
+const collection = db.collection<WithId<RPC>>("rpc");
 
 export async function POST(req: NextRequest) {
   try {
-    let request = await req.json();
-    let db = client.db("centiweb");
-    let collection = db.collection<WithId<RPC>>("rpc");
-    let doc = await collection.findOne({ addr: request.addr });
+    const request = await req.json();
+    const doc = await collection.findOne({ addr: request.addr });
 
     if (!doc) {
       await collection.insertOne(request);
@@ -22,6 +22,42 @@ export async function POST(req: NextRequest) {
   } catch (e) {
     return NextResponse.json({
       status: "error: " + e,
+    });
+  }
+}
+
+export async function GET() {
+  try {
+    let addresses: RPC[] = [];
+    const cursor = collection.find({});
+
+    await cursor.forEach((doc) => {
+      addresses.push({ addr: doc.addr });
+    });
+
+    return NextResponse.json({
+      status: "success",
+      data: addresses,
+    });
+  } catch (e) {
+    return NextResponse.json({
+      status: "error: " + e,
+      data: [],
+    });
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    let rpc = req.nextUrl.searchParams.get("addr");
+    collection.deleteOne({ addr: rpc?.toString() });
+
+    return NextResponse.json({
+      status: "success",
+    });
+  } catch (e) {
+    return NextResponse.json({
+      status: "errpr: " + e,
     });
   }
 }
