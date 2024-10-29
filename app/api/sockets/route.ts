232644ddfd208e13b3af6389b11873d8dddd9552
mohammad <mgharebaghi92@gmail.com>
2024-10-29 19:09:07 +0330

    add websocket connection

--- app/api/sockets/route.ts
+++ app/api/sockets/route.ts
@@ -30,6 +30,6 @@ wss.on('connection', (ws: WebSocket) => {
 export async function GET() {
   return NextResponse.json({ 
     status: 'ok',
-    wsEndpoint: 'wss://centichain.org:8080'
+    wsEndpoint: 'ws://localhost:8080'
   })
 }
