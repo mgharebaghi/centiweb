8b0ebe09e66194ff75ecdc5a4bf6c9e2ea482de2
mohammad <mgharebaghi92@gmail.com>
2024-10-30 18:22:12 +0330

    remove websocket connection

--- app/components/menu/index.tsx
+++ app/components/menu/index.tsx
@@ -54,50 +54,7 @@ function Menu() {
   useEffect(() => {
     setActiveItem(pathname ?? "");
     fetchCoins();
-    socketInitializer();
-
-    return () => {
-      if (ws) {
-        ws.close();
-      }
-    };
   }, [pathname]);
-
-  const socketInitializer = async () => {
-    try {
-      const response = await fetch("/api/sockets");
-      const data = await response.json();
-
-      const websocket = new WebSocket(data.wsEndpoint);
-
-      websocket.onopen = () => {
-        console.log("Connected to WebSocket server");
-      };
-
-      websocket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        if (data.type === "block") {
-          console.log("Received Reward:", data.data);
-        }
-
-        if (data.type === "transaction") {
-          console.log("Received Status:", data.data);
-        }
-      };
-
-      websocket.onerror = (error) => {
-        console.error("WebSocket error:", error);
-      };
-
-      websocket.onclose = () => {
-        console.log("Disconnected from WebSocket server");
-      };
-
-      setWs(websocket);
-    } catch (error) {
-      console.error("Failed to initialize WebSocket:", error);
-    }
-  };
 
   const fetchCoins = async () => {
     try {
