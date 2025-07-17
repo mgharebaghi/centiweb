318d6660ddedee6309e610632b2d45ce799ff7ff
mohammad <mgharebaghi92@gmail.com>
2025-07-17 22:52:04 +0330

    change V version to 0.34.1

--- app/download/components/validators.tsx
+++ app/download/components/validators.tsx
@@ -36,7 +36,7 @@ function Validators() {
     {
       icon: FaWindows,
       text: "Windows",
-      link: "https://centichain.org/downloads/updates/windows/x64/v0.33.12/Centichain_0.33.12_x64_en-US.msi",
+      link: "https://centichain.org/downloads/updates/windows/x64/v0.34.1/Centichain_0.34.1_x64_en-US.msi",
       available: true,
       requirements: "Windows 10 or later, 64-bit processor required",
       size: "15 MB",
